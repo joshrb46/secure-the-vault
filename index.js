16431da1987bf69e0e55a73a556a9af443cc9368
joshrb46 <joshrb46@gmail.com>
2026-04-20 11:21:23 -0700

    fix number 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ code1 = 7 + 3;
 const code2 = -10 + 100 / 2;
 const code3 = (39 % 10) + 30;
 const message = "The vault has been secured. The combination is:";
-const codeA = code1 - code2 - code3;
+const codeA = code1 + code2 + code3;
 const codeB = `${code1}-${code2}-${code3}`;
 console.log(message);
 console.log(codeA);
